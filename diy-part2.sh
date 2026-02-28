7732c55ad733ab76c719036bd40cac3fe4fa111c
fans3g22 <104512665+fans3g22@users.noreply.github.com>
2026-02-28 14:02:08 +0800

    Remove conflicting mbedtls and clean up libraries
    Remove conflicting mbedtls source and clean up other libraries.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 强行删除插件包里冲突的 mbedtls 源码
+rm -rf feeds/packages/libs/mbedtls
+rm -rf feeds/kenzo/mbedtls
+rm -rf feeds/small/mbedtls
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 顺便清理一下可能会导致其他冲突的加密库（可选但推荐）
+rm -rf feeds/packages/libs/openssl
+rm -rf feeds/packages/libs/wolfssl
