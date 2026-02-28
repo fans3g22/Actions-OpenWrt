3656f8cd63151613c7621404ebbca381db33084b
fans3g22 <104512665+fans3g22@users.noreply.github.com>
2026-02-28 11:36:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,6 +21,10 @@
 # echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki' >> feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-src-git packages https://github.com/immortalwrt/packages.git
-src-git luci https://github.com/immortalwrt/luci.git
-src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki
+#!/bin/bash
+
+# 格式：echo "配置内容" >> feeds.conf.default
+
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki" >> feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
